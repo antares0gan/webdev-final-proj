e919fc2636bac6d88fc19b3984e69532657358c0
Peng Gan <gan.pe@husky.neu.edu>
2020-04-22 13:04:05 -0700

    temp work

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -49,7 +49,7 @@ function inFlight(state = false, action) {
 
 function redirect(state = '', action) {
   if (action.type === 'LOGIN_SUCCESS' || action.type === 'REGISTER_SUCCESS') {
-    return '/home/';
+    return '/home';
   }
   return '';
 }
